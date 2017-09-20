ca82d6814d3878efc247872f00fe2a6fbdfa2364
Collin Werner <collinw9898@gmail.com>
2017-09-19 21:55:24 -0400

    reworked the site, new features and whatnot

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-var myNumber = 0;
-
-function myFunction() {
-    var text = document.getElementById('myText').value;
-
-    myNumber = myNumber + text;
-
-    document.getElementById('output').innerHTML = myNumber + text;
-}
-
-var newOutput = '';
-
-function newBoi(thing) {
-    var num = document.getElementById('myText').value;
-
-    if (num > 5)
-        alert('ha ha, no.');
-    else
-        alert('ha ha, small boi');
+function rotate() {
+    document.getElementById("box").classList.toggle('rotate');
 }
